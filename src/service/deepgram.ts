72ed5f8eff205f2be597c7a257b4256036b44be6
shubham bajaj <shmbajaj32@gmail.com>
2024-03-13 17:52:43 +0530

    feat: created deepgram connection service

---
+++ src/service/deepgram.ts
@@ -0,0 +1,23 @@
+import { createClient } from "@deepgram/sdk";
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+import { $TSFixMe } from "types/common";
+import { DEEPGRAM_API_KEY } from "../utils/config";
+
+export const connectDeepgram = async () => {
+  try {
+    if (!DEEPGRAM_API_KEY) {
+      throw Error("Deepgram API Key is Missing");
+    }
+    const deepgram = createClient(DEEPGRAM_API_KEY);
+    return deepgram.listen.live({
+      model: "enhanced-phonecall",
+      smart_format: true,
+      encoding: "mulaw",
+      sample_rate: 8000,
+      channels: 1,
+    });
+  } catch (err: $TSFixMe) {
+    const message = err?.message || "Failed to Connect with Deepgram";
+    throw Error(message);
+  }
+};
--- src/service/openai.ts
+++ src/service/openai.ts
@@ -3,7 +3,7 @@ import { ChatCompletionMessageParam } from "openai/resources/index.mjs";
 import { MODELS, ResponseType } from "types/openai";
 //TODO: fix no-unused-vars for $TSFixMe type
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
-import { $TSFixMe } from "../types/common";
+import type { $TSFixMe } from "../types/common";
 import { OPEN_AI_KEY } from "../utils/config";
 import { systemPrompt } from "../utils/data";
 
@@ -35,7 +35,7 @@ export const connectOpenAI = async () => {
 
 export const agent = async (
   openai: OpenAI,
-  userInput: string,
+  userInput: string
 ): Promise<AgentResponse> => {
   messages.push({ role: "user", content: `${userInput}` });
   const response = await openai.chat.completions.create({
@@ -48,7 +48,7 @@ export const agent = async (
   const assistantPrompt = response.choices[0].message?.content;
   messages.push({ role: "assistant", content: assistantPrompt });
   const GPT_RESPONSE = JSON.parse(
-    assistantPrompt || `${FALLBACK_RESPONSE}`,
+    assistantPrompt || `${FALLBACK_RESPONSE}`
   ) as AgentResponse;
   return GPT_RESPONSE;
 };
