79e2a182fcee165c64f2fc45f91776a79522bba3
shubham bajaj <shmbajaj32@gmail.com>
2024-04-05 01:00:04 +0530

    chore[14]: send transcription on when puncatuation with eos.

--- backend/src/service/transcription-service.ts
+++ backend/src/service/transcription-service.ts
@@ -60,10 +60,11 @@ export class TranscriptionService extends EventEmitter {
 
           if (text) {
             console.log(`deepgram: transcript ${text}`);
+
             if (
-              PUNCTUATION_TERMINATORS.includes(text.slice(-1)) ||
-              transcription.speech_final ||
-              PUNCTUATION_TERMINATORS.includes(this.finalResult.slice(-1))
+              PUNCTUATION_TERMINATORS.includes(text.slice(-1)) &&
+              (transcription.speech_final ||
+                PUNCTUATION_TERMINATORS.includes(this.finalResult.slice(-1)))
             ) {
               this.finalResult += `${text}`;
               this.emit("transcription", this.finalResult);
