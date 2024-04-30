4885ab9a4f6809340edb0faff1b44216ed23f8ff
shubham bajaj <shmbajaj32@gmail.com>
2024-04-30 17:01:13 +0530

    feat: navigate ivr using decision tree

---
+++ backend/src/service/ivr-service.ts
@@ -0,0 +1,81 @@
+import EventEmitter from "events";
+import { BaseResponse, ResponseType } from "types/openai";
+import { colorInfo, colorSuccess, colorWarn } from "utils/colorCli";
+import { ActiveCallConfig } from "./activecall-service";
+import { redisClient } from "./redis";
+
+interface IVRMenu {
+  intent: string;
+  triggers: string[];
+  response: string;
+}
+
+export class IVRService extends EventEmitter {
+  private ivrMenu: Array<IVRMenu>;
+  private callSid: string;
+  private lastOptionIntent: string;
+
+  constructor(ivrMenu: Array<IVRMenu>) {
+    super();
+    this.ivrMenu = ivrMenu;
+    this.callSid = "";
+    this.lastOptionIntent = ivrMenu.at(ivrMenu.length - 1)?.intent || "";
+  }
+
+  setCallSid(callSid: string) {
+    this.callSid = callSid;
+  }
+
+  public handleResponse(transcript: string) {
+    this.updateIVRTranscription({
+      role: "user",
+      content: transcript,
+    });
+    const transcriptFormatted = transcript
+      .toLowerCase()
+      .replace(/[^a-zA-Z0-9\s]/g, "");
+    console.info(colorInfo(`ivrservice: ${transcriptFormatted}`));
+    const match = this.ivrMenu.find((menu) =>
+      menu.triggers.some((trigger) => transcriptFormatted.includes(trigger))
+    );
+    if (!match) {
+      console.log(colorWarn(`ivrservice: No Match for: ${transcript}`));
+      return;
+    }
+    console.log(
+      colorSuccess(`ivrservice: Match, ${match.response} for: ${transcript}`)
+    );
+    const ivrReply: BaseResponse = {
+      content: match.response,
+      responseType:
+        match.response.at(match.response.length - 1) === "#" ||
+        !Number.isNaN(+match.response)
+          ? ResponseType.SEND_DIGITS
+          : ResponseType.SAY_FOR_VOICE,
+    };
+    if (
+      match.intent === this.lastOptionIntent &&
+      !ActiveCallConfig.getInstance().getCallConfig()?.isLastIvrMenuOptionUsed
+    ) {
+      console.log(
+        colorInfo(
+          `ivrservice ->  lastOptionIntent: ${this.lastOptionIntent}, matchIntent: ${match.intent}`
+        )
+      );
+      ActiveCallConfig.getInstance().setIsLastIVRMenuOptionUsed();
+    }
+    this.updateIVRTranscription({
+      role: "assistant",
+      content: match.response,
+    });
+    this.emit("ivrreply", ivrReply);
+  }
+
+  updateIVRTranscription(message: {
+    content: string;
+    role: "user" | "assistant";
+  }) {
+    const serializedMessage = JSON.stringify(message);
+    redisClient.rPush(`${this.callSid}__ivr--transcription`, serializedMessage);
+  }
+}
