6cf134ce89e69d718b14a476f158254532548805
shubham bajaj <shmbajaj32@gmail.com>
2024-04-30 16:53:44 +0530

    feat: check if call is transfered to custome rep

---
+++ backend/src/scripts/iscall-transfered.ts
@@ -0,0 +1,40 @@
+import OpenAI from "openai";
+import { MODELS } from "types/openai";
+import { colorErr, colorInfo } from "utils/colorCli";
+import { OPEN_AI_KEY } from "utils/config";
+import { callTransferAnalyzer } from "utils/data";
+
+export const isCallTransfered = async (userInput: string) => {
+  try {
+    const openaiClient = new OpenAI({ apiKey: OPEN_AI_KEY });
+    const completeion = await openaiClient.chat.completions.create({
+      messages: [
+        {
+          role: "system",
+          content: callTransferAnalyzer,
+        },
+        {
+          role: "user",
+          content: userInput,
+        },
+      ],
+      model: MODELS.GPT_4_TUBRO,
+      temperature: 0,
+      max_tokens: 3,
+    });
+    const message = completeion.choices[0].message.content;
+    const isTransfered = message === "Yes" || message === "yes";
+    console.log(
+      colorInfo(
+        `call transfered -> isTransfered:${isTransfered} message: ${message} transcription:${userInput}`
+      )
+    );
+    return isTransfered;
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  } catch (err: any) {
+    console.log(
+      colorErr(err?.message || "Failed to analyze is Call Transfered.")
+    );
+    return false;
+  }
+};
