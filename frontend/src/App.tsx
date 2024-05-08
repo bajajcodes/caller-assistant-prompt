c70ae828426081ad9c033abb49d8145ff7f607c0
shubham bajaj <shmbajaj32@gmail.com>
2024-05-08 20:32:00 +0530

    refactor: removed call context

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,16 +1,13 @@
 import "./App.css";
-import { CallProvider } from "./components/call-context";
 import { CallLog } from "./components/call-log";
 import { MakeCallForm } from "./components/make-call";
 
 function App() {
   return (
-    <CallProvider>
-      <main className="min-h-screen w-full grid max-w-4xl grid-cols-1 gap-6 px-4 py-6 mx-auto  lg:gap-10 md:grid-cols-2 ">
-        <MakeCallForm />
-        <CallLog />
-      </main>
-    </CallProvider>
+    <main className="min-h-screen w-full px-4 py-6 mx-auto  grid gap-4 lg:gap-10 md:grid-cols-2 ">
+      <MakeCallForm />
+      <CallLog />
+    </main>
   );
 }
 
