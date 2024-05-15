a3ceec855b1855d382255333564bf6dcf2d8a8c5
shubham bajaj <shmbajaj32@gmail.com>
2024-05-15 20:57:21 +0530

    refactor: update status and call ended by

--- backend/src/scripts/hangup-call.ts
+++ backend/src/scripts/hangup-call.ts
@@ -1,3 +1,4 @@
+import { CallLogKeys, CallLogService } from "@service/calllog-service";
 import twillio from "twilio";
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 import { $TSFixMe } from "types/common";
@@ -6,7 +7,9 @@ import { TWILIO_ACCOUNT_SID, TWILIO_AUTH_TOKEN } from "utils/config";
 export const hangupCall = async (callSid: string) => {
   try {
     const client = new twillio.Twilio(TWILIO_ACCOUNT_SID, TWILIO_AUTH_TOKEN);
-    await client.calls(callSid).update({ status: "completed" });
+    const update = await client.calls(callSid).update({ status: "completed" });
+    CallLogService.create(callSid, CallLogKeys.CALL_STATUS, update.status);
+    CallLogService.create(callSid, CallLogKeys.CALL_ENDED_BY, "/hangupcall");
     console.info("twilio: hangup call done.");
   } catch (err: $TSFixMe) {
     const reason = err?.message;
